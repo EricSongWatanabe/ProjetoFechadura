3743158d8d1fd73d6d86cc031f47225d68d3d56e
VictorPimentelLario <146137307+VictorPimentelLario@users.noreply.github.com>
2024-10-14 18:56:07 +0000

    adicionado parcialmente funcao de verificar

--- fechadura.asm
+++ fechadura.asm
@@ -1,4 +1,4 @@
-; --- Mapeamento de Hardware (8051) ---
+ ; --- Mapeamento de Hardware (8051) ---
     RS      equ     P1.3    ;Reg Select ligado em P1.3
     EN      equ     P1.2    ;Enable ligado em P1.2
 
@@ -6,21 +6,43 @@
     LJMP START
 
 START:
-    MOV 20h, #00h
-    MOV 21h, #00h
-    MOV 22h, #00h
-    MOV 23h, #00h
-    MOV 24h, #00h
-    MOV R5, #00H
+	MOV 20h, #00h
+  	MOV 21h, #00h
+  	MOV 22h, #00h
+  	MOV 23h, #00h
+  	MOV 24h, #00h
+  	MOV R5, #00H
+	MOV 50h, #00h
+	MOV 51h, #00h
+	MOV 52h, #00h
+	MOV 53h, #00h
+	MOV 54h, #00h
 
 MAIN:
+	CLR P2.7
+	MOV A, R5
+	CJNE A, #00h , conferirSenha
 	ACALL leituraTeclado
-	JNB F0, MAIN     ; Se F0 não está setado, continua verificando teclado
+	JNB F0, MAIN
 	ACALL MOVER_20
 	MOV A, R5
 	CJNE A, #00h, CADASTRO
-	CLR F0           ; Limpa o flag F0
-	SJMP MAIN        ; Loop principal
+	CLR F0
+	SJMP MAIN
+
+conferirSenha:
+	MOV R2, #33h
+	SETB P2.7
+	ACALL leituraTeclado
+	JNB F0, conferirSenha
+	ACALL MOVER_50
+	MOV A, R5
+	CLR F0
+	SJMP conferirSenha
+	RET
+
+ChamarMAIN:
+	LJMP MAIN
 
 MOVER_20:
 	MOV A, 20H
@@ -57,50 +79,86 @@ MOVER_24:
 	RET
 
 CADASTRO:
-	clr P1.3
+	clr P1.5
 	RET
 
 leituraTeclado:
-    MOV R0, #0       ; Reseta R0
-    MOV P0, #0FFh    ; Configura todas as linhas em alto
+  	MOV R0, #0       ; Reseta R0
+  	MOV P0, #0FFh    ; Configura todas as linhas em alto
 
     ; Varre as linhas do teclado matricial
-    CLR P0.0         ; Limpa a linha 0
-    CALL colScan     ; Chama a sub-rotina para verificar colunas
-    JB F0, fimScan   ; Se F0 está setado, pula para o fim
+  	CLR P0.0         ; Limpa a linha 0
+  	CALL colScan     ; Chama a sub-rotina para verificar colunas
+  	JB F0, fimScan   ; Se F0 está setado, pula para o fim
 
-    SETB P0.0        ; Configura a linha 0 em alto
-    CLR P0.1         ; Limpa a linha 1
-    CALL colScan     ; Chama a sub-rotina para verificar colunas
-    JB F0, fimScan   ; Se F0 está setado, pula para o fim
+  	SETB P0.0        ; Configura a linha 0 em alto
+  	CLR P0.1         ; Limpa a linha 1
+  	CALL colScan     ; Chama a sub-rotina para verificar colunas
+	JB F0, fimScan   ; Se F0 está setado, pula para o fim
 
-    SETB P0.1        ; Configura a linha 1 em alto
-    CLR P0.2         ; Limpa a linha 2
-    CALL colScan     ; Chama a sub-rotina para verificar colunas
-    JB F0, fimScan   ; Se F0 está setado, pula para o fim
+	SETB P0.1        ; Configura a linha 1 em alto
+	CLR P0.2         ; Limpa a linha 2
+	CALL colScan     ; Chama a sub-rotina para verificar colunas
+	JB F0, fimScan   ; Se F0 está setado, pula para o fim
 
-    SETB P0.2        ; Configura a linha 2 em alto
-    CLR P0.3         ; Limpa a linha 3
-    CALL colScan     ; Chama a sub-rotina para verificar colunas
-    JB F0, fimScan   ; Se F0 está setado, pula para o fim
-
+ 	SETB P0.2        ; Configura a linha 2 em alto
+  	CLR P0.3         ; Limpa a linha 3
+  	CALL colScan     ; Chama a sub-rotina para verificar colunas
+	JB F0, fimScan   ; Se F0 está setado, pula para o fim
+		
 fimScan:
-    RET
+	RET
 
 colScan:
-    JNB P0.4, keyFound ; Verifica coluna 0
-    INC R0            ; Incrementa R0 se não encontrado
-    JNB P0.5, keyFound ; Verifica coluna 1
-    INC R0            ; Incrementa R0 se não encontrado
-    JNB P0.6, keyFound ; Verifica coluna 2
-    INC R0            ; Incrementa R0 se não encontrado
-    RET
+	JNB P0.4, keyFound ; Verifica coluna 0
+	INC R0            ; Incrementa R0 se não encontrado
+	JNB P0.5, keyFound ; Verifica coluna 1
+	INC R0            ; Incrementa R0 se não encontrado
+	JNB P0.6, keyFound ; Verifica coluna 2
+  	INC R0            ; Incrementa R0 se não encontrado
+ 	RET
 
 keyFound:
-    SETB F0           ; Seta F0 indicando tecla encontrada
-    RET
+	SETB F0           ; Seta F0 indicando tecla encontrada
+	RET
 
+
+MOVER_50:
+	MOV A, 50H
+	CJNE A, #00H, MOVER_51
+	MOV 50h, R0
+	ACALL delay
+	RET      
+
+MOVER_51:
+	MOV A, 51H
+	CJNE A, #00H, MOVER_52
+	MOV 51h, R0
+	ACALL delay
+	RET  
+
+MOVER_52:
+	MOV A, 52H
+	CJNE A, #00H, MOVER_53
+	MOV 52h, R0
+	ACALL delay
+	RET
+
+MOVER_53:
+	MOV A, 53H
+	CJNE A, #00H, MOVER_54
+	MOV 53h, R0
+	MOV R5, #01H
+	ACALL delay
+	RET
+
+MOVER_54:
+	MOV 54h, R0
+	ACALL delay
+	RET	
+
+	
 delay:
-MOV R7, #50
-DJNZ R7, $
-RET
+	MOV R7, #50
+	DJNZ R7, $
+	RET
